ef7c3c1f6c109024cd586ba583799a73f0364842
unknown <larissateixeira998@gmail.com>
2025-07-03 11:51:33 -0300

    Importação corrigida

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,24 @@
-import { forwardRef, Module } from "@nestjs/common";
-import { JwtModule } from "@nestjs/jwt";
-import { PassportModule } from "@nestjs/passport";
-import { UsuarioModule } from "../usuario/usuario.module";
-import { Bcrypt } from "./bcrypt/bcrypt";
-import { jwtConstants } from "./constants/constants";
-import { AuthController } from "./controllers/auth.controller";
-import { AuthService } from "./services/auth.service";
-import { LocalStrategy } from "./strategy/local.strategy";
+import { forwardRef, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsuarioModule } from '../usuario/usuario.module';
+import { Bcrypt } from './bcrypt/bcrypt';
+import { jwtConstants } from './constants/constants';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './services/auth.service';
+import { LocalStrategy } from './strategy/local.strategy';
 
 @Module({
-    imports: [
-        forwardRef(() => UsuarioModule),
-        PassportModule,
-        JwtModule.register({
-            secret: jwtConstants.secret,
-            signOptions: {expiresIn: "1h"},
-        })
-    ],
-    providers: [Bcrypt, AuthService, LocalStrategy],
-    controllers: [AuthController],
-    exports: [Bcrypt],
+  imports: [
+    forwardRef(() => UsuarioModule),
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [Bcrypt, AuthService, LocalStrategy],
+  controllers: [AuthController],
+  exports: [Bcrypt],
 })
-export class AuthModule {};
+export class AuthModule {}
--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,17 +1,23 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from "@nestjs/common";
-import { AuthService } from "../services/auth.service";
-import { UsuarioLogin } from "../entities/usuariologin.entity";
-import { LocalAuthGuard } from "../guard/local-auth.guard";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { UsuarioLogin } from '../entities/usuariologin.entity';
+import { LocalAuthGuard } from '../guard/local-auth.guard';
+import { AuthService } from '../services/auth.service';
 
-@Controller("/usuarios")
+@Controller('/usuarios')
 export class AuthController {
-    constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
-    @UseGuards(LocalAuthGuard)
-    @HttpCode(HttpStatus.OK)
-    @Post('/logar')
-    login(@Body() usuario: UsuarioLogin): Promise<any> {
-        return this.authService.login(usuario);
-    }
-
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Post('/logar')
+  login(@Body() usuario: UsuarioLogin): Promise<any> {
+    return this.authService.login(usuario);
+  }
 }
