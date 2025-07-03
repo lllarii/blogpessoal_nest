a9fbc91043580f71edceeb79f853c6d3d3376609
unknown <larissateixeira998@gmail.com>
2025-07-02 21:22:22 -0300

    Completando o módulo de Usuário

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -6,25 +6,26 @@ import { Postagem } from "../../postagem/entities/postagem.entity";
 @Entity ({name: "tb_usuarios"})
 export class Usuario {
     @PrimaryGeneratedColumn()
-    id: number
+    id: number;
 
     @IsNotEmpty()
     @Column({length:255, nullable: false})
-    nome: string
+    nome: string;
     
-    @IsEmail()
+    @IsEmail() //confere se o q foi digitado segue o padrão de e-mail com "@", ".co" etc.
     @IsNotEmpty()
     @Column({length: 255, nullable: false})
-    usuario: string
+    usuario: string;
 
-    @MinLength(8)
+    @MinLength(8) //mínimo de caracteres aceito
+    //@MaxLength(12) limita a quantidade de caracteres do usuário, o que não é tão recomendado para senhas; depende da situação 
     @IsNotEmpty()
     @Column({length: 255, nullable: false })
-    senha: string
+    senha: string;
 
-    @Column({length: 5000 })
-    foto: string
+    @Column({length: 5000 }) 
+    foto: string;
 
     @OneToMany(() => Postagem, (postagem) => postagem.usuario)
-    postagem: Postagem[]
+    postagem: Postagem[];
 }
